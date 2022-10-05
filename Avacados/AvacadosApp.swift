d5c3c70445f342dae2f70295423f86fcad02d196
ozturkbasrii <ozturkbasrii@gmail.com>
2022-10-05 13:20:47 +0300

    Initial Commit

---
+++ Avacados/AvacadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvacadosApp.swift
+//  Avacados
+//
+//  Created by Hasan Basri Öztürk on 5.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AvacadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
